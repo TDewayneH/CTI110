a90ae2e4bcab9f36871095a861a794f682b0c117
TDewayneH <33096829+TDewayneH@users.noreply.github.com>
2017-11-01 10:53:02 -0400

    Add files via upload
    Start M4

---
+++ CTI-110/M4-Input Processing and Output/M4T1_Hicks.py
@@ -0,0 +1,25 @@
+#CTI-110
+#M4T1 - Sales Prediction
+#Dewayne Hicks
+#1NOV17
+#
+
+"""
+    This code takes the input from the user to calculate the profit
+"""
+totalSales = int(input("Please input the projected total sales(whole dollar amount): $"))
+print(\
+      """
+        Calculating one moment please
+      """)
+
+
+"""
+    This is the code to calculate the profit and print it for the user
+"""
+annualProfit = totalSales * 0.23
+print('Your profit for the year is $' + str(annualProfit))
+
+
+#Just to keep it up until the user is done with the program
+input('\n\nPress ENTER to exit program.')
