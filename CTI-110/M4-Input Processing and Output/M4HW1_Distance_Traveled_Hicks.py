27a1bc1e730828292d083e22ddc2d4807b0828ec
TDewayneH <33096829+TDewayneH@users.noreply.github.com>
2017-11-06 10:56:41 -0500

    Add files via upload
    Uploaded M4HW1

---
+++ CTI-110/M4-Input Processing and Output/M4HW1_Distance_Traveled_Hicks.py
@@ -0,0 +1,13 @@
+#CTI-110
+#M4HW1 - Distance Traveled
+#Terry Hicks
+#6NOV17
+
+speed = 70
+disAfter6 = speed * 6
+disAfter10 = speed * 10
+disAfter15 = speed * 15
+
+print("The distance the car will travel in 6 hours is", disAfter6)
+print("The distance the car will travel in 10 hours is", disAfter10)
+print("The distance the car will travel in 15 hours is", disAfter15)
