98ca1e40dc2c86b3e60af4d0c496a5e287e08377
TDewayneH <33096829+TDewayneH@users.noreply.github.com>
2017-11-08 10:55:43 -0500

    Add files via upload
    Uploaded M5 and M5T1

---
+++ CTI-110/M5-Decision Structures/M5T1_AreasOfRectangles_Hicks.py
@@ -0,0 +1,24 @@
+#M5T1_Hicks
+#Areas of rectangles
+#Dewayne Hicks
+#8NOV17
+
+rect1Length = int(input("Please input the lenght of the first rectangle: "))
+rect1Width = int(input("Please input the widht of the first rectangle: "))
+
+rect2Lenght = int(input("Please input the lenght of the second rectangle: "))
+rect2Width = int(input("Please input the width of the second rectangle: "))
+
+
+rect1 = rect1Length*rect1Width
+rect2 = rect2Lenght*rect2Width
+
+if(rect1>rect2):
+    print("The first rectangle has the greater area")
+elif(rect1<rect2):
+    print("The second rectangle has the greater area")
+else:
+    print("They have the same area")
+
+
+input("\n\nPress enter to exit")
