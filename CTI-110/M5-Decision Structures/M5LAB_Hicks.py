5d11231b78b6e3c6214365dfd52c1c33efc790bf
TDewayneH <33096829+TDewayneH@users.noreply.github.com>
2017-11-08 11:34:30 -0500

    Add files via upload
    Uplaoded M5LAB

---
+++ CTI-110/M5-Decision Structures/M5LAB_Hicks.py
@@ -0,0 +1,39 @@
+#M5LAB
+#Debugging
+#Dewayne Hicks
+#8NOV17
+
+def main():
+    # This program takes a number grade and outputs a letter grade.
+
+    # system uses 10-point grading scale
+    A_score = 90
+    B_score = 80
+    C_score = 70
+    D_score = 60
+    F_score = 50
+
+    
+    score = int(input('Enter grade: '))
+
+    if score >= A_score:
+        print('Your grade is: A')
+    elif score < A_score and score >= B_score:
+        print('Your grade is: B')
+    elif score < B_score and score >= C_score:
+        print('Your grade is: C')
+    elif score < C_score and score >= D_score:
+        print('Your grade is: D')
+    else:
+        print('Your grade is: F')
+
+
+
+
+
+
+
+# program start
+main()
+
+input("\n\nPress enter to exit.")
