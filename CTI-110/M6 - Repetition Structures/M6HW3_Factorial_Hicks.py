e186bf03798152670162deebab50a4497d9bcd35
TDewayneH <33096829+TDewayneH@users.noreply.github.com>
2017-11-15 10:50:41 -0500

    Updated M6HW3

--- CTI-110/M6 - Repetition Structures/M6HW3_Factorial_Hicks.py
+++ CTI-110/M6 - Repetition Structures/M6HW3_Factorial_Hicks.py
@@ -11,13 +11,22 @@ def main():
     print("The factorial of", nonNegative, "is", total)
 
 def factorial(number):
+    num = 1
+    while number != 1:
+        num = num * number
+        number = number - 1
+    return num
+        
+    
+"""
     if number == 0:
         return 1
     else:
         return number * factorial(number-1)
-        
+"""        
 
 main()
 
 
 input("\n\nPress enter to exit.")
+
