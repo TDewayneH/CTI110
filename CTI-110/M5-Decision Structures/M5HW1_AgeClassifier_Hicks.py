febe5e564c756f25abc8dc4a425611547cb7bc77
TDewayneH <33096829+TDewayneH@users.noreply.github.com>
2017-11-20 10:54:12 -0500

    Update M5HW1_AgeClassifier_Hicks.py

--- CTI-110/M5-Decision Structures/M5HW1_AgeClassifier_Hicks.py
+++ CTI-110/M5-Decision Structures/M5HW1_AgeClassifier_Hicks.py
@@ -6,19 +6,21 @@
 def main():
     
     #Ask user their age
-    age = int(input("What is your age padawan, if you are younger than 1 round up also how are you using a computer?!?: "))
+    age = int(input("What is your age padawan, if you are younger than 1 round"\
+                    " up also how are you using a computer?!?: "))
 
     #If user is one or younger
     if(age<=1):                 
         print("You are an infant and the darkside shall consume you!")
         
     #If user is between 1 and 13
-    elif(age>1 and age<13):     
+    elif(age<13):     
         print("You are a child and the darkside can still consume you!")
         
     #if user is between 13 and 20
-    elif(age>=13 and age<20):   
-        print("You are a teenager and might be able to fight the darkside for a time!")
+    elif(age<20):   
+        print("You are a teenager and might be able to fight the darkside for"\
+              " a time!")
         
     #if user is 20 or over
     else:                       
